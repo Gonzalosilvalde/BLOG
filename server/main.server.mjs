798175087394f37a8e83ac180ae20d1dae739d35
Gonzalosilvalde <72249639+Gonzalosilvalde@users.noreply.github.com>
2024-09-04 00:37:46 +0200

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-DEPKM7UD.mjs";import"./chunk-BCJPJJJ5.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-JI44XTEB.mjs";import"./chunk-BCJPJJJ5.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
